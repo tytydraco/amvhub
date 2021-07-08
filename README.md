ddb16afe8f0306144a71a7b75543916a5b8f025f
Tyler Nijmeh <tylernij@gmail.com>
2021-07-08 15:46:17 -0700

    Update method of conversion
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ A program to bulk convert videos into AMV format for Chinese DAPs
 
 # How-To
 1. Put media in input folder
-2. `./amvhub`
+2. `./amvhub <file>`
--- amvhub.sh
+++ amvhub.sh
@@ -1,68 +1,27 @@
 #!/usr/bin/env bash
 shopt -s globstar
-cd "$(dirname "$0")" || exit 1
 
-# ---
-export WIDTH="320"
-export HEIGHT="-1"
-export FPS="15"
-export FORMAT="amv"
-# ---
-export INPUT="../TV Shows"
-export OUTPUT="output"
-# ---
-LOCK=".lock"
-LOG="log.txt"
-JOBS=4
+WIDTH="160"
+HEIGHT="128"
+FPS="15"
 
-mkdir -p "$OUTPUT"
+if [[ -z "$1" ]]
+then
+  echo "No video file specified"
+  exit 1
+fi
 
-while [[ -f "$LOCK" ]]
-do
-    echo "Waiting for lock to release [at: $LOCK]..."
-    sleep 1
-done
-
-touch "$LOCK"
-date >> "$LOG"
-
-_amvhub_convert() {
-    local output_file
-    local output_dir
-
-    output_file="$(echo "$1" | sed "s|^$INPUT/|$OUTPUT/|")"
-    echo "$output_file"
-    output_file="${output_file%.*}.$FORMAT"
-
-    [[ -f "$output_file" ]] && return
-
-    output_dir="${output_file%/*}"
-    mkdir -p "$output_dir"
-
-    ffmpeg \
-        -i "$1" \
-        -f "$FORMAT" \
-        -vf "mpdecimate,scale=$WIDTH:$HEIGHT" \
-        -strict -1 \
-        -r "$FPS" \
-        -ac 1 \
-        -ar 22050 \
-        -block_size 1470 \
-        -n \
-        -qmin 3 \
-        -qmax 3 \
-        "$output_file"
-}
-
-export -f _amvhub_convert
-find "$INPUT" -type f -not -name "*.$FORMAT" -print0 | xargs -0 -P "$JOBS" -I {} bash -c '_amvhub_convert "{}"'
-
-wait
-
-rm -f "$LOCK"
-
-echo ""
-echo -e "\033[0;31mFinished amvhub.\033[0m"
-echo ""
-
-exit 0
+OUTPUT_FILE="${1%.*}.amv"
+ffmpeg \
+    -i "$1" \
+    -f "amv" \
+    -vf "mpdecimate,scale=$WIDTH:$HEIGHT,crop=in_w:$HEIGHT" \
+    -strict -1 \
+    -r "$FPS" \
+    -ac 1 \
+    -ar 22050 \
+    -block_size 1470 \
+    -n \
+    -qmin 3 \
+    -qmax 3 \
+    "$OUTPUT_FILE"
